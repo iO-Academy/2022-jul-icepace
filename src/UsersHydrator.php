57d5ca8381cc670753a3a4614f13a96f6967fea9
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2022-09-12 15:49:34 +0100

    created userhydrator public function

--- src/UsersHydrator.php
+++ src/UsersHydrator.php
@@ -4,5 +4,11 @@ namespace Icepace;
 
 class UsersHydrator
 {
-
+    public static function getAllUsers(PDO $db)
+    {
+        $query = $db->prepare('SELECT * FROM `users`');
+        $query->setFetchMode(PDO::FETCH_CLASS, User::class);
+        $query->execute();
+        return $query->fetchAll();
+    }
 }
