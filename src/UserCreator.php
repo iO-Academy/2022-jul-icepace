2c20f75b2b8cf08a65a8ae748916fdf8fdc6b3b1
Georgeoak03 <101403429+Georgeoak03@users.noreply.github.com>
2022-09-13 16:20:35 +0100

    the start of story

---
+++ src/UserCreator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Icepace;
+
+use PDO;
+
+class UserCreator
+{
+    function addToDb(array $newUser)
+    {
+        $username = $newUser['username'];
+        $password = $newUser['hashed_pass'];
+        $bio = $newUser['bio'];
+
+        $connectionString = 'mysql:host=db; dbname=icepace';
+        $dbUsername = 'root';
+        $dbPassword = 'password';
+        $db = new PDO($connectionString, $dbUsername, $dbPassword);
+        $queryString = 'INSERT INTO  `users` (`username`, `hashed_pass`, `bio`)
+    VALUES (:username, :hashed_pass, :bio)';
+        $query = $db->prepare($queryString);
+        $query->execute (['username' => $username, 'hashed_pass' => $password, 'bio' => $bio]);
+        return
+    }
+}
