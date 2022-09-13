140789b8a78340298e9c38601c02828979a615da
Harry <virtualslide@gmail.com>
2022-09-13 15:02:49 +0100

    changes to UserTest as per review comments

--- test/UserTest.php
+++ test/UserTest.php
@@ -1,8 +1,7 @@
 <?php
-
+require_once 'vendor/autoload.php';
 use Icepace\User;
 use PHPUnit\Framework\TestCase;
-require_once 'vendor/autoload.php';
 
 class UserTest extends TestCase
 {
@@ -12,12 +11,5 @@ class UserTest extends TestCase
         $result = $user->createUserCardHtml();
         $expected = '<div class="userCard"><img class="avatarImg" src="fakeAvatar.jpeg" alt="Profile Picture"><p class="cardUsernameText">fakeUsername</p></div>';
         $this->assertEquals($expected, $result);
-    }
-
-    public function testSuccessCreateUserCardHtml2()
-    {
-        $user = new User(1, 5, 'fakeHashedPass', 'fake bio', 3);
-        $result = $user->createUserCardHtml();
-        $this->assertEquals('<div class="userCard"><img class="avatarImg" src="3" alt="Profile Picture"><p class="' . "cardUsernameText" . '">5</p></div>', $result);
     }
 }
