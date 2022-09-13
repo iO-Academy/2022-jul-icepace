c03fe47c66a6531ab4427abdf6ed4b7d36c8bfb5
Mike Oram <Mikeoram01@gmail.com>
2022-09-13 09:59:07 +0100

    Added the user_profile.scss file and imported the index.css file into it. Created the boilerplate for the HTML in the user_profile.php page.

--- user_profile.php
+++ user_profile.php
@@ -9,8 +9,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="index.css" />
+    <link rel="stylesheet" type="text/css" href="user_profile.css" />
     <title>Icepace</title>
 </head>
 <body>
+    <nav>
+        <h1>Icepace</h1>
+    </nav>
 </body>
 </html>
