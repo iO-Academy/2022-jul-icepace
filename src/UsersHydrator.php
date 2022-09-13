80f4b753f90fcc6b051a448dd3144d1f742199a3
Mike Oram <Mikeoram01@gmail.com>
2022-09-13 07:38:19 +0100

    Removed the print_r from index.php that was used for testing.
    Changed the query to use the column names and not *

--- src/UsersHydrator.php
+++ src/UsersHydrator.php
@@ -8,7 +8,7 @@ class UsersHydrator
 {
     public static function getAllUsers(PDO $db): array
     {
-        $query = $db->prepare('SELECT * FROM `users`');
+        $query = $db->prepare('SELECT `id`, `username`, `hashed_pass`, `bio`, `avatar` FROM `users`');
         $query->setFetchMode(PDO::FETCH_CLASS, User::class);
         $query->execute();
         return $query->fetchAll();
