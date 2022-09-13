05f12482c977c4b73d73de35b18b860ca4a469bb
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2022-09-13 13:52:20 +0100

    static function getUserById

--- src/UsersHydrator.php
+++ src/UsersHydrator.php
@@ -13,4 +13,12 @@ class UsersHydrator
         $query->execute();
         return $query->fetchAll();
     }
+
+    public static function getUserById(PDO $db, int $id)
+    {
+        $query = $db->prepare('SELECT `id`, `username`, `hashed_pass`, `bio`, `avatar` FROM `users` WHERE id= ?');
+        $query->setFetchMode(PDO::FETCH_CLASS, User::class);
+        $query->execute([$id]);
+        return $query->fetchAll();
+    }
 }
