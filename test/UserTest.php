209954274e4bc1a6fad53a1a8bed2b36dcdc99fb
Mike Oram <Mikeoram01@gmail.com>
2022-09-14 12:15:24 +0100

    Updated the tests following gods advice

--- test/UserTest.php
+++ test/UserTest.php
@@ -10,14 +10,16 @@ class UserTest extends TestCase
     {
         $user = new User(1,'fakeUsername', 'fakeHashedPass', 'fake bio', 'fakeAvatar.jpeg');
         $result = $user->createUserCardHtml();
-        $this->assertEquals('<a class="profileLink" href="./userProfile.php?username=fakeUsername"><div class="userCard"><img class="avatarImg" src="./assets/imgs/avatars/fakeAvatar.jpeg" alt="Profile Picture"><p class="cardUsernameText">fakeUsername</p></div></a>', $result);
+        $expected = '<a class="profileLink" href="./userProfile.php?username=fakeUsername"><div class="userCard"><img class="avatarImg" src="./assets/imgs/avatars/fakeAvatar.jpeg" alt="Profile picture" /><p class="cardUsernameText">fakeUsername</p></div></a>';
+        $this->assertEquals($expected, $result);
     }
 
     public function testSuccessSanitizedBio()
     {
         $user = new User(1,'fakeUsername', 'fakeHashedPass', '<img src="https://media1.giphy.com/media/Vuw9m5wXviFIQ/200w.gif" />', 'fakeAvatar.jpeg');
         $result = $user->getSanitizedBio();
-        $this->assertEquals('', $result);
+        $expected = '';
+        $this->assertEquals($expected, $result);
     }
 
     public function testSuccessGetFullAvatarPath()
