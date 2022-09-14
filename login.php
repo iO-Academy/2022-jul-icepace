685688ba2fc42f0851d5b97765231d8cb61cb30b
Mike Oram <Mikeoram01@gmail.com>
2022-09-14 16:32:50 +0100

    HTML boilerplate added

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="index.css"/>
+    <title>Icepace</title>
+</head>
+<body>
+<nav>
+    <h1>Icepace</h1>
+</nav>
+<div class="loginPageContainer">
+    <div>
+        <h2 class="loginTitle">Login</h2>
+        <form class="loginForm" action="">
+            <div>
+                <label for="usernameInput">Username:</label>
+                <input type-="text" class="usernameInput" id="usernameInput" />
+            </div>
+            <div>
+                <label for="passwordInput">Password:</label>
+                <input type-="password" class="passwordInput" id="passwordInput" />
+            </div>
+            <button type="submit" class="submitForm" >Submit</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
+
