c711b9f5a40126de18094e55bce88030482fe08b
Harry <virtualslide@gmail.com>
2022-09-14 09:43:44 +0100

    setup the UserCreator class

---
+++ src/UserCreator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Icepace;
+
+class UserCreator
+{
+    public static function validateInput()
+    {
+
+    }
+
+    private static function validateUsername()
+    {
+
+    }
+
+    private static function validatePassword()
+    {
+
+    }
+
+    private static function validateBio()
+    {
+
+    }
+
+    public static function insertUserIntoDb()
+    {
+
+    }
+}
