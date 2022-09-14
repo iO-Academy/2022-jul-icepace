addf076687dcf4fe2284ed8ff6ac1f8b0312f889
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2022-09-14 09:37:39 +0100

    bio sanitized and unit test created

--- test/UserTest.php
+++ test/UserTest.php
@@ -12,4 +12,11 @@ class UserTest extends TestCase
         $result = $user->createUserCardHtml();
         $this->assertEquals('<a class="profileLink" href="./userProfile.php?username=fakeUsername"><div class="userCard"><img class="avatarImg" src="./assets/imgs/avatars/fakeAvatar.jpeg" alt="Profile Picture"><p class="cardUsernameText">fakeUsername</p></div>', $result);
     }
+
+    public function testSuccessSanitizedBio()
+    {
+        $user = new User(1,'fakeUsername', 'fakeHashedPass', '<img src="https://media1.giphy.com/media/Vuw9m5wXviFIQ/200w.gif" />', 'fakeAvatar.jpeg');
+        $result = $user->getSanitizedBio();
+        $this->assertEquals('', $result);
+    }
 }
