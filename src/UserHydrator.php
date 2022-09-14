33949d7322edd043acff3333151b6d7adc2c0a9f
Mike Oram <Mikeoram01@gmail.com>
2022-09-14 12:11:04 +0100

    Resolved the missing backticks.

--- src/UserHydrator.php
+++ src/UserHydrator.php
@@ -13,10 +13,10 @@ class UserHydrator
         $query->execute();
         return $query->fetchAll();
     }
-
+ 
     public static function getUserByUsername(PDO $db, string $username): User
     {
-        $query = $db->prepare('SELECT `id`, `username`, `hashed_pass`, `bio`, `avatar` FROM `users` WHERE username= ?');
+        $query = $db->prepare('SELECT `id`, `username`, `hashed_pass`, `bio`, `avatar` FROM `users` WHERE `username`= ?');
         $query->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, User::class);
         $query->execute([$username]);
         return $query->fetch();
