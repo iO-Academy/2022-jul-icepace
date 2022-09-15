aab020007be1f19eef35acccdff057384d1ac6a2
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2022-09-15 14:31:57 +0100

    created editUser.php

---
+++ editUser.php
@@ -0,0 +1,47 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="index.css"/>
+    <link rel="stylesheet" type="text/css" href="login.css" />
+    <title>Icepace</title>
+</head>
+<body>
+<nav>
+    <a class="homePageTitle" href="index.php">
+        <h1>Icepace</h1>
+    </a>
+    <div class="navBarButton">
+        <div class="buttonContainer">
+            <a class="navButton" href="login.php">My Profile</a>
+        </div>
+        <div class="buttonContainer">
+            <a class="navButton" href="">Logout</a>
+        </div>
+    </div>
+</nav>
+<div class="editPageContainer">
+    <div class="loginPageFormContainer">
+        <h2 class="loginTitle">Edit profile</h2>
+        <form class="editForm" action="">
+            <div class="inputContainer">
+                <label class="inputLabel" for="usernameInput">Username</label>
+                <input type-="text" class="usernameInput" id="usernameInput" />
+            </div>
+            <div class="inputContainer">
+                <label class="inputLabel" for="passwordInput">Password</label>
+                <input type-="password" class="passwordInput" id="passwordInput" />
+            </div>
+            <div class="inputContainer">
+            <input class="submitForm" type="submit" value="Login to Icepace!">
+            </div>
+        </form>
+    </div>
+</div>
+</body>
+</html>
