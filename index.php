b450a12c008d56bcaebefac6a95311727d3fad43
Josh Bennet <josh@thebennets.net>
2022-09-20 10:59:10 +0100

    banana

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $users = UserHydrator::getAllUsers($db);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="index.css" />
-    <title>Icepace</title>
+    <title>Icepace banana</title>
 </head>
 <body>
     <nav>
